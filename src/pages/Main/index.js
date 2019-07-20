a69f970ac1ba4257dd940802a6312531f209d454
zaqueu lima dos santos <zaqueulima1@gmail.com>
2019-07-20 19:13:24 -0300

    Applies style to main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithub, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
   return (
-    <Title error={false}>
-      Main
-      <small>menor</small>
-    </Title>
+    <Container>
+      <h1>
+        <FaGithub /> Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
